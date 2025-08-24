a01775e57c8d6c0704f75ee9d112e620201d405a
Monika D <monikadev2004@gmail.com>
2025-08-24 14:37:15 +0530

    added usercourse progress

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/user.js"
 import { Purchase } from "../models/Purchase.js";
 import Stripe from "stripe";
 import Course from "../models/Course.js";
+import { CourseProgress } from "../models/CourseProgress.js";
 
 // Get user data
 export const getUserData=async(req,res)=>{
@@ -80,6 +81,35 @@ export const purchaseCourse=async(req,res)=>{
         })
         res.json({success:true,session_url:session.url})
         
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
+}
+
+// Update user course progress
+export const updateUserCourseProgress=async(req,res)=>{
+    try {
+        const userId=req.auth.userId
+        const {courseId,lectureId}=req.body
+        const progressData=await CourseProgress.findOne({userId,courseId})
+
+        if(progressData){
+            if(progressData.lectureCompleted.includes(lectureId)){
+                return res.json({success:true,message:'Lecture Already Completed'})
+            }
+
+            progressData.lectureCompleted.push(lectureId)
+            await progressData.save()
+        }
+        else{
+            await CourseProgress.create({
+                userId,
+                courseId,
+                lectureCompleted:[lectureId]
+            })
+        }
+
+        res.json({success:true,message:'Progress Updated'})
     } catch (error) {
         res.json({success:false,message:error.message})
     }
